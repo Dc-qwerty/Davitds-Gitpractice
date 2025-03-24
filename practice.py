5d699c3ea495c7ef0c844dde9cb06eb3fe7157cc
Dc-qwerty <ngangudavidclement23@gmail.com>
2025-03-24 15:41:42 +0300

    repeat1

--- practice.py
+++ practice.py
@@ -1,9 +1,3 @@
 print("hello world")
 print("WRC25")
 print("data science at moringa")
-<<<<<<< HEAD
-print("best WRC2025")
-=======
-print("what a wonderful WRC2025")
->>>>>>> 168c69e554b81870e7fc1bbd10b48242e23a4b0a
-print("1")
